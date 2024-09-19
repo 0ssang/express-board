af7e02d3d97ea62a64ed822ab90676d80abfddc3
조영상 <51320595+0ssang@users.noreply.github.com>
2024-09-20 00:54:52 +0900

    test: app.js 동작 확인
    템플릿 엔진 설정 및 기본 레이아웃 파일 /views/layouts/main.handlebar 작성

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const handlebars = require("express-handlebars")
+const app = express()
+
+// template engine 등록
+app.engine("handlebars", handlebars.engine())
+
+// 웹 페이지 로드 시 사용할 템플릿 엔진 설정
+app.set("view engine", "handlebars")
+// 뷰 디렉토리 설정
+app.set("views", __dirname + "/views")
+
+// 라우터 설정
+app.get("/", (req, res) => {
+    res.render("home", {title: "안녕하세요", message: "방문을 환영합니다!"})
+})
+
+app.listen(3000)
